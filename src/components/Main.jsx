3aaf1633e12524ae3208a201b44467de331001a7
alonso-estevam <alonso_estevam@outlook.com>
2022-08-29 21:21:17 -0300

    fix: requisição

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Main = () => {
-  
-  const[info, setInfo] = useState()
+  const [info, setInfo] = useState();
+
+  const [valorInput, setValorInput] = useState("");
+
+  function handleChange(e) {
+    setValorInput(e.target.value);
+    console.log(valorInput);
+  }
+
   async function handleRequisicao() {
     const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=zn3IsgyhbVyTpMQrm5CJ7Gb4vWdzdpHcPqw1fc3U&date=`
+      `https://api.nasa.gov/planetary/apod?api_key=zn3IsgyhbVyTpMQrm5CJ7Gb4vWdzdpHcPqw1fc3U&date=${valorInput}
+      `
     );
     const json = await response.json();
     console.log(json);
     setInfo(json);
   }
 
-
-
   return (
     <div>
-      <form action="">
-        <label htmlFor="">Digite uma data</label>
-        <input type="number" name="" id="" />
-        <button onClick={handleRequisicao}>Buscar</button>
+      <form>
+        <label htmlFor="date">Digite uma data</label>
+        <input
+          type="date"
+          id="date"
+          value={valorInput}
+          onChange={handleChange}
+        />
       </form>
       <img src={info ? info.url : ""} alt="" />
-      
+      <br></br>
+      <p></p>
+      <button onClick={handleRequisicao}>Enviar</button>
     </div>
   );
 };
